5c1f6a80f9f0b8b3ade9ea130fc11cc1acb56f2d
taruc <tanhongzhi98@gmail.com>
2019-12-26 16:06:19 +0800

    Initial

--- app/src/main/java/com/example/useless/MainActivity.kt
+++ app/src/main/java/com/example/useless/MainActivity.kt
@@ -1,24 +1,47 @@
 package com.example.useless
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
 
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var usefulViewModel: UsefulViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
+        //https://codelabs.developers.google.com/codelabs/android-room-with-a-view-kotlin/#11
+        //Initialize the RecyclerView adapter
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+        //Initialize the ViewModel
+
+        fab.setOnClickListener {
+            val intent = Intent(this, AddActivity::class.java)
+            startActivityForResult(intent, REQUEST_CODE)
+
+            //Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG).setAction("Action", null).show()
         }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if(requestCode == REQUEST_CODE){
+            if (resultCode == Activity.RESULT_OK){
+                //TODO : Save a useful record to the database
+                Toast.makeText(this,"Record saved", Toast.LENGTH_SHORT).show()
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data)
+    }
+    companion object{
+        const val REQUEST_CODE = 1
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
---
+++ app/src/main/java/com/example/useless/UsefulViewModel.kt
@@ -0,0 +1,26 @@
+package com.example.useless
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+
+class UsefulViewModel (application: Application):AndroidViewModel(application){
+    //Maintain a reference to the Repository
+    private val usefulRepository:UsefulRepository
+
+    //Maintain a copy of the useful record
+    val allUseful : LiveData<List<Useful>>
+
+    init {
+        val usefulDao = UsefulDatabase.getDatabase(application).usefulDao()
+        usefulRepository = UsefulRepository(usefulDao)
+        allUseful = usefulRepository.allUseful
+    }
+
+    //Create a co-routine function to insert data in background thread
+    fun insertUseful(useful: Useful) = viewModelScope.launch {
+        usefulRepository.insertUseful(useful)
+    }
+}
